bf4ecd4f52b9238b1a30f79401e3e7ca33ab1c92
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-10-19 12:52:40 +0200

    Finished part 2.

---
+++ Part2/server/src/resolvers.js
@@ -0,0 +1,15 @@
+const resolvers = {
+  Query: {
+    // return an array of tracks for home page
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
+
+module.exports = resolvers;
